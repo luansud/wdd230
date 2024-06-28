dfedccc167bcd452d09fee94b6a908fefb6c35b2
luansud <luanfmedeiros@hotmail.coM>
2024-06-28 10:42:24 -0300

    Learning Activity 1 and 2

---
+++ week03/scripts/getDate.js
@@ -0,0 +1,11 @@
+const showCurrentlyYear = document.getElementById('year');
+const currentYear = new Date();
+showCurrentlyYear.innerHTML = "<span>&copy </span>"+currentYear.getFullYear();
+
+
+const lastModifiedString = document.lastModified;
+const lastModifiedDate = new Date(lastModifiedString);
+const formattedDate = lastModifiedDate.toLocaleDateString() + ' ' + lastModifiedDate.toLocaleTimeString();
+
+const lastModifiedText = document.getElementById("lastModified");
+lastModifiedText.textContent = " Last Modified:" + formattedDate;
