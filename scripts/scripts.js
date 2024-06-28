5eed333f3d5af051bc549db8d0e6c1d92778bc11
luansud <luanfmedeiros@hotmail.com>
2024-06-28 20:54:48 -0300

    Team Project Finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+// HAMBURGUER MENU
 const hamburgerElement = document.querySelector("#hamburguerButton");
 const navElement = document.querySelector(".menuNav");
 
@@ -5,7 +6,7 @@ hamburgerElement.addEventListener("click", () =>{
     navElement.classList.toggle("open");
     hamburgerElement.classList.toggle("open");
 });
-
+// Dark Mode
 const darkButton = document.querySelector(".darkButton");
 const bodyAll = document.querySelector("body");
 const root = document.documentElement;
@@ -22,3 +23,14 @@ darkButton.addEventListener("click", () =>{
     root.classList.toggle('open');
 
 });
+
+// COUTING VISITS
+const visitsDisplay = document.querySelector("#visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = ` This is your first visit.`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
