c229b57bda689d005f929b9b2d364a1e968e99f1
luansud <luanfmedeiros@hotmail.coM>
2024-07-15 16:26:17 -0300

    Finishing TeamActivity

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,10 +27,46 @@ darkButton.addEventListener("click", () =>{
 // COUTING VISITS
 const visitsDisplay = document.querySelector("#visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = ` This is your first visit.`;
+if(visitsDisplay){
+    if (numVisits !== 0) {
+        visitsDisplay.textContent = numVisits;
+    } else {
+        visitsDisplay.textContent = 'This is your first visit.';
+    }
+    numVisits++;
+    localStorage.setItem("numVisits-ls", numVisits);
 }
-numVisits++;
-localStorage.setItem("numVisits-ls", numVisits);
+
+// Range Page Rating
+const rangeValue = document.getElementById("rangeValue");
+const range = document.getElementById("pageRating");
+
+range.addEventListener('change',showRange);
+range.addEventListener("input",showRange);
+
+function showRange(){
+    rangeValue.innerHTML = range.value;
+    console.log("teste Range");
+    
+}
+console.log("teste");
+
+// checking if Passwords Match
+
+const userPassword = document.getElementById("password");
+const confirmPassword = document.getElementById("cPassword");
+const errorPassword = document.getElementById("errorPassword");
+
+confirmPassword.addEventListener("focusout", matchingPasswords);
+
+function matchingPasswords(){
+    if(userPassword.value != confirmPassword.value){
+        errorPassword.textContent = "The passwords don't match! Please type the same password";
+        userPassword.value = "";
+        confirmPassword.value = "";
+        userPassword.focus();
+    } else {
+        errorPassword.textContent = "";
+    }
+}
+
