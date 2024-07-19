a98e7dd8c5b555401f0897bd3310b0a13aaa228f
luansud <luanfmedeiros@hotmail.coM>
2024-07-19 16:56:52 -0300

    Team Activity Done

--- scripts/links.js
+++ scripts/links.js
@@ -7,7 +7,6 @@ async function getLinks(linksURL){
         const data = await response.json()
         displayLinks(data);
     }
-
 }
 
 function displayLinks(data){
@@ -15,48 +14,19 @@ function displayLinks(data){
     const weeks = data.weeks
     for (let w = 0; w < weeks.length; w++) {
         const week = weeks[w]
-        console.log(week)
-        console.log(week.week)
-
         const li = document.createElement('li')
         li.className = "linkColor"
-        li.textContent = week.week + ":"
+        li.textContent = week.week + ": "
         listLinks.appendChild(li)
 
         week.links.forEach(link => {
             const a = document.createElement('a')
             a.href = link.url
-            a.textContent = link.title
+            a.textContent = " " + link.title + " | "            
+            li.appendChild(a)
 
-
-
-
-
-
-            console.log(link.title)
-            console.log(link.url)
         });
     }
 }
-
-
-
-
-
-
-
-// criar li, 
-// li com classe linkColor
-// li recebe texto week:value
-// li recebe  a (links[0])
-// href do a do li recebe links[0].url
-// a do li recebe links links[0].title
-// recebe |
-
-
-
-
-
-
 
 getLinks(linksURL);
